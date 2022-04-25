4ec1ea804831dee4919cd0103e4e50c3c3837226
Nick <galaznicolas12@gmail.com>
2022-04-25 16:22:25 -0400

    archivo corregido

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", function (req, res) {
             "lechuga",
             "tomate",
         ],
-        Bienvenida:"Bienvenido al mercado WEB, seleccione sus productos"
+        Bienvenida: "Bienvenido al mercado WEB, seleccione sus productos"
     });
 });
 
